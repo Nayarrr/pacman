72b5f3ecd49c1c433865a84e869c98ed03f42dfc
Nayarrr <votre-email@exemple.com>
2026-06-12 16:22:07 +0800

    feat : add way attribute

--- character.cs
+++ character.cs
@@ -3,10 +3,13 @@ public class Character
     public required string name;
     public double speed;
 
-    public Character(string name, double speed)
+    public Way way;
+
+    public Character(string name, double speed, Way way)
     {
         this.name = name;
         this.speed = speed;
+        this.way = way;
     }
 
 }
